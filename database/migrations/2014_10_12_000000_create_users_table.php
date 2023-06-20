b3cc3571477d380f46dc0e4fe3997b493ec2e45e
mraihannayuna <m.raihannayua13@gmail.com>
2023-06-20 14:31:45 +0700

    first

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('roles')->enum('ADMIN','SELLER','USER')->default('USER');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', [LandingController::class, 'index'])->name('landing');
+Route::resource('shop', ShopController::class);
+Route::resource('transaction', TransactionController::class);
